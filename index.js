a51351782373bdcb5f79d14b5a960da339f2559e
Francisco Presencia <public@francisco.io>
2019-12-08 18:21:07 +0900

    Improved cookie parsing by taking some logic bits from 'cookie'

--- index.js
+++ index.js
@@ -148,13 +148,50 @@ var bodyParser = async ctx => {
   ctx.body = await parseBody(ctx.req);
 };
 
-var cookieParser = ctx => {
-  ctx.cookies = {};
-  const cookie = ctx.headers.cookie;
-  if (!cookie) return;
+const decode = str => {
+  try {
+    return decodeURIComponent(str).catch(err => str);
+  } catch (e) {
+    return str;
+  }
 };
 
-const decode = decodeURIComponent;
+// Extracted originally from npm's "cookie"
+const parse = str => {
+  if (typeof str !== "string") {
+    throw new TypeError("argument str must be a string");
+  }
+
+  return str.split(/; */).reduce((cookies, pair) => {
+    const eq_idx = pair.indexOf("=");
+
+    // skip things that don't look like key=value
+    if (eq_idx < 0) return cookies;
+
+    const key = pair.substr(0, eq_idx).trim();
+    const val = pair.substr(eq_idx + 1).trim();
+
+    // unquote any quoted value
+    if ('"' == val[0]) {
+      val = val.slice(1, -1);
+    }
+
+    // only assign once; the first time
+    if (undefined == cookies[key]) {
+      cookies[key] = decode(val);
+    }
+    return cookies;
+  }, {});
+};
+
+var cookieParser = ctx => {
+  ctx.cookies = {};
+  if (!ctx.headers.cookie) return;
+
+  ctx.cookies = parse(ctx.headers.cookie);
+};
+
+const decode$1 = decodeURIComponent;
 
 // Parse the query from the url (without the `?`)
 const parseQuery = (query = "") => {
@@ -163,7 +200,7 @@ const parseQuery = (query = "") => {
     .split("&")
     .filter(Boolean)
     .map(p => p.split("="))
-    .reduce((all, [key, val]) => ({ ...all, [decode(key)]: decode(val) }), {});
+    .reduce((all, [key, val]) => ({ ...all, [decode$1(key)]: decode$1(val) }), {});
 };
 
 // Available in Node globally since 10.0.0
--- src/middleware/cookieParser.js
+++ src/middleware/cookieParser.js
@@ -1,5 +1,42 @@
+const decode = str => {
+  try {
+    return decodeURIComponent(str).catch(err => str);
+  } catch (e) {
+    return str;
+  }
+};
+
+// Extracted originally from npm's "cookie"
+const parse = str => {
+  if (typeof str !== "string") {
+    throw new TypeError("argument str must be a string");
+  }
+
+  return str.split(/; */).reduce((cookies, pair) => {
+    const eq_idx = pair.indexOf("=");
+
+    // skip things that don't look like key=value
+    if (eq_idx < 0) return cookies;
+
+    const key = pair.substr(0, eq_idx).trim();
+    const val = pair.substr(eq_idx + 1).trim();
+
+    // unquote any quoted value
+    if ('"' == val[0]) {
+      val = val.slice(1, -1);
+    }
+
+    // only assign once; the first time
+    if (undefined == cookies[key]) {
+      cookies[key] = decode(val);
+    }
+    return cookies;
+  }, {});
+};
+
 export default ctx => {
   ctx.cookies = {};
-  const cookie = ctx.headers.cookie;
-  if (!cookie) return;
+  if (!ctx.headers.cookie) return;
+
+  ctx.cookies = parse(ctx.headers.cookie);
 };
