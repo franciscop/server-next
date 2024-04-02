34a11e613dbb070bb36e9766c6a2ea4f9fb3b255
Francisco <public@francisco.io>
2024-04-02 12:59:28 +0900

    Some basic types

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,31 @@
-declare module "server" {
-  type ServerOptions = {
-    port?: number;
-  };
-  type Context = {
-    body: {};
-    headers: {};
-    url: URL & { params: {}; query: {} };
-  };
+type ServerOptions = {
+  port?: number;
+};
+type Context = {
+  body: {};
+  headers: {};
+  url: URL & { params: {}; query: {} };
+};
 
-  type Middleware = (ctx: Context) => any;
+type Middleware = (ctx: Context) => any;
 
-  export interface Server {
-    new (options?: ServerOptions): this;
+type Router = {};
 
-    get(url: string, ...middleware: Middleware[]): this;
-  }
+declare interface Server {
+  (options?: ServerOptions): this;
 
-  const server: Server;
-  export default server;
+  socket(url: string, ...middleware: Middleware[]): this;
+  get(url: string, ...middleware: Middleware[]): this;
+  head(url: string, ...middleware: Middleware[]): this;
+  post(url: string, ...middleware: Middleware[]): this;
+  put(url: string, ...middleware: Middleware[]): this;
+  patch(url: string, ...middleware: Middleware[]): this;
+  del(url: string, ...middleware: Middleware[]): this;
+  options(url: string, ...middleware: Middleware[]): this;
+
+  use(url: string, ...middleware: Middleware[]): this;
+  router(router: Router): this;
 }
+
+declare const server: Server;
+export default server;
