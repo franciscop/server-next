2de927482b74c69dc0b46f1bfd4f15174539d206
Francisco <public@francisco.io>
2024-04-02 13:05:25 +0900

    Some basic types

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,29 @@
+type Bucket = {};
+
+type Method =
+  | "socket"
+  | "get"
+  | "head"
+  | "post"
+  | "put"
+  | "patch"
+  | "delete"
+  | "options";
+
 type ServerOptions = {
   port?: number;
+  views?: string | Bucket;
+  public?: string | Bucket;
+  uploads?: string | Bucket;
 };
+
 type Context = {
-  body: {};
-  headers: {};
+  method: Method;
+  headers: { [key: string]: string | string[] };
+  cookies: { [key: string]: any };
+  body?: any;
   url: URL & { params: {}; query: {} };
+  options: ServerOptions;
 };
 
 type Middleware = (ctx: Context) => any;
@@ -14,16 +33,16 @@ type Router = {};
 declare interface Server {
   (options?: ServerOptions): this;
 
-  socket(url: string, ...middleware: Middleware[]): this;
-  get(url: string, ...middleware: Middleware[]): this;
-  head(url: string, ...middleware: Middleware[]): this;
-  post(url: string, ...middleware: Middleware[]): this;
-  put(url: string, ...middleware: Middleware[]): this;
-  patch(url: string, ...middleware: Middleware[]): this;
-  del(url: string, ...middleware: Middleware[]): this;
-  options(url: string, ...middleware: Middleware[]): this;
+  socket(path: string, ...middleware: Middleware[]): this;
+  get(path: string, ...middleware: Middleware[]): this;
+  head(path: string, ...middleware: Middleware[]): this;
+  post(path: string, ...middleware: Middleware[]): this;
+  put(path: string, ...middleware: Middleware[]): this;
+  patch(path: string, ...middleware: Middleware[]): this;
+  del(path: string, ...middleware: Middleware[]): this;
+  options(path: string, ...middleware: Middleware[]): this;
 
-  use(url: string, ...middleware: Middleware[]): this;
+  use(...middleware: Middleware[]): this;
   router(router: Router): this;
 }
 
--- index.types.ts
+++ index.types.ts
@@ -1,3 +1,13 @@
 import server from ".";
 
-server();
+server()
+  .get("/", (ctx) => {
+    console.log(ctx.method);
+    console.log(ctx.url);
+    console.log(ctx.headers);
+    console.log(ctx.cookies);
+    console.log(ctx.body);
+  })
+  .post("/", () => {
+    return 201;
+  });
