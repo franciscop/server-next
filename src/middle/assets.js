217ef7e155c1e67b417d51c253902376ac55f7bb
Francisco <public@francisco.io>
2024-07-03 03:45:33 +0900

    Couple of small fixes

--- src/middle/assets.js
+++ src/middle/assets.js
@@ -3,7 +3,8 @@ import { type } from "../reply.js";
 export default async function assets(ctx) {
   try {
     // TODO: streaming
-    const asset = await ctx.options.public.read(ctx.url.pathname);
+    // Read it as buffer (null)
+    const asset = await ctx.options.public.read(ctx.url.pathname, null);
     if (!asset) return;
     return type(ctx.url.pathname.split(".").pop()).send(asset);
   } catch (error) {
--- src/pathPattern.js
+++ src/pathPattern.js
@@ -1,4 +1,6 @@
 export default function pathPattern(pattern, path) {
+  if (pattern === "*") return {};
+
   pattern = "/" + pattern.replace(/^\//, "");
   pattern = pattern.replace(/\/$/, "") || "/";
   path = path.replace(/\/$/, "") || "/";
