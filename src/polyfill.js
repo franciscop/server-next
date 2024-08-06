baa03f5a2a7b518e002c4695f08a687d6492177f
Francisco <public@francisco.io>
2024-08-06 21:51:02 +0200

    Fix globalThis

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,17 +1,17 @@
 // out = new Response(out, { headers: { "content-type": type } });
 if (typeof Response === "undefined") {
-  global.Response = function Response(body, other = {}) {
+  globalThis.Response = function Response(body, other = {}) {
     return { body, ...other };
   };
 }
 
 // Polyfill Netlify's environment variables
 if (typeof Netlify !== "undefined") {
-  if (!global.process) {
-    global.process = {};
+  if (!globalThis.process) {
+    globalThis.process = {};
   }
-  if (!global.process.env) {
-    global.process.env = {};
+  if (!globalThis.process.env) {
+    globalThis.process.env = {};
   }
   Object.assign(global.process.env, Netlify.env.toObject());
 }
